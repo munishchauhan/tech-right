8914cdd56555848dc42af7a9605034177602e5d5
munishchauhan <chauhan.munish1@gmail.com>
2021-04-26 08:31:30 +0530

    toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 import { RegisterVolunteerComponent } from './volunteers/register-volunteer/register-volunteer.component';
 import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
@@ -23,7 +25,13 @@ import { HttpClientModule } from '@angular/common/http';
     ReactiveFormsModule,
     AppRoutingModule,
     NgbModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 5000,
+      positionClass: 'toast-top-center',
+      preventDuplicates: true,
+    }), // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/students/student-add/student-add.component.ts
+++ src/app/students/student-add/student-add.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { formatDate } from '@angular/common';
 import { Student } from 'src/app/_models/student.model';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-student-add',
@@ -26,7 +27,8 @@ export class StudentAddComponent implements OnInit {
   // dateOfBirth: any = this.formattedDate;
 
   constructor(
-    private httpClient: HttpClient
+    private httpClient: HttpClient,
+    private toastr: ToastrService
   ) { }
 
   ngOnInit(): void {
@@ -40,16 +42,22 @@ export class StudentAddComponent implements OnInit {
     this.httpClient.post('http://localhost:8080/student/register', this.student)
       .subscribe(
         {
-          next: resp => {
-            console.log(resp);
+          next: (resp: any) => {
+            //console.log(resp);
+            if (resp.id > 0) {
+              this.toastr.success('Student added');
+            } else {
+              this.toastr.error('Student could not be added');
+            }
           },
           error: err => {
-            console.log(err);
+            // console.log(err);
+            this.toastr.error('Student could not be added');
           }
         }
       );
 
-      }
+  }
 
   reset() {
     // this.firstName = this.lastName = '';
